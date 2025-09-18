bf69faa73c8a9d75893be0ba9f3c46ccb7f1abd9
Pabitra Muduli <pkmuduli001@gmail.com>
2025-09-19 01:05:12 +0530

    minor fixes

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
-import app from "./src/app.js";
+import express from "express";
+import bodyParser from "body-parser";
+import orderRoutes from "./routes/order.routes.js";
+
+const app = express();
+app.use(bodyParser.json());
+
+// Routes
+app.use("/orders", orderRoutes);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`✅ Order Service running on port ${PORT}`));
