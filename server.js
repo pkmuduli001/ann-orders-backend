7a374f62fd8b90d68f1823ff5c17eb08385561b7
Pabitra Muduli <pkmuduli001@gmail.com>
2025-09-19 11:50:25 +0530

    minor fixes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
-import orderRoutes from "./routes/order.routes.js";
+import orderRoutes from "./src/routes/order.routes.js";
 
 const app = express();
 app.use(bodyParser.json());
