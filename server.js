3479d926515ef19bc4e4f92466aedfd4c3f56473
Pabitra Muduli <pkmuduli001@gmail.com>
2025-09-19 13:14:33 +0530

    fixing cors issue

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
 import orderRoutes from "./src/routes/order.routes.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 // Routes
 app.use("/orders", orderRoutes);
